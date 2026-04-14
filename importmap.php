8e9b628c9ba375248c9ed273996b2046548957a7
Florent <florent.destremau@gmail.com>
2026-04-14 14:15:05 +0200

    Migrate Bootstrap and Bootstrap Icons from CDN to AssetMapper

--- importmap.php
+++ importmap.php
@@ -53,4 +53,18 @@ return [
         'version' => '2.5.2',
         'type' => 'css',
     ],
+    'bootstrap' => [
+        'version' => '5.3.8',
+    ],
+    '@popperjs/core' => [
+        'version' => '2.11.8',
+    ],
+    'bootstrap/dist/css/bootstrap.min.css' => [
+        'version' => '5.3.8',
+        'type' => 'css',
+    ],
+    'bootstrap-icons/font/bootstrap-icons.min.css' => [
+        'version' => '1.13.1',
+        'type' => 'css',
+    ],
 ];
